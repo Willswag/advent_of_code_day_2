58150b7b42b9109385677906698fb5446fc97814
Spencer Waguespack <wswaguespack403@gmail.com>
2022-12-02 18:16:12 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let path = Path::new("input.txt");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldnt open {}: {}",display,why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why)=> panic!("could read {}: {}",display,why),
+        Ok(_)=> print!("contains:data\n" ),
+    }}
