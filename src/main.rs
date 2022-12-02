a768666b7159ce15f027bae913dd6ff5c68f86d1
Spencer Waguespack <wswaguespack403@gmail.com>
2022-12-02 18:36:23 -0500

    got the righrt data

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -13,11 +15,27 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why)=> panic!("could read {}: {}",display,why),
-        Ok(_)=> print!("contains:data\n{}" ,s),
+        Ok(_)=> print!("contains:data\n" ),
     }
-    
+
+
+    let split_sting = s.split("\n");
+
+    let plays = split_sting.collect::<Vec<&str>>();
+
+    for p in plays{
+        print!("{}\n",p);
+        sim_game(p);
+    }
 
 
 }
 
+fn sim_game(game_inputs : &str) {
+    let elf_in = game_inputs.chars().nth(0).unwrap();
+    let my_input = game_inputs.chars().nth(2).unwrap();
 
+    print!("the other guy put {} I put {}\n",elf_in,my_input);
+
+
+}
